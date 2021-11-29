14ce7eb069776ef48390fbb424ac68f7f7339073
Carlos Enrique Brito Rodriguez <cbrito1994@gmail.com>
2021-11-28 21:51:21 -0600

    corrected

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-// import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+const express = require('express');
+const app = express();
+const path = require('path');
 
-let scene, camera, renderer;
-// console.log(THREE)
-console.log(OrbitControls)
+app.use(express.static(__dirname + '/public'))
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
+
+app.listen(3000, () => console.log('Visit http://localhost:3000'))
+// app.listen(3000, () => console.log('Visit http://127.0.0.1:3000'))
---
+++ public/client.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+let scene, camera, renderer;
+console.log(THREE)
+console.log(OrbitControls)
