ccaae30b6a5551c04894b1172701e0c7e34d7790
Carlos Enrique Brito Rodriguez <cbrito1994@gmail.com>
2021-11-28 22:17:17 -0600

    usdzexporter

--- public/client.js
+++ public/client.js
@@ -1,6 +1,8 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { USDZExporter } from 'three/examples/jsm/exporters/USDZExporter';
 
 let scene, camera, renderer;
 console.log(THREE)
 console.log(OrbitControls)
+console.log(USDZExporter)
